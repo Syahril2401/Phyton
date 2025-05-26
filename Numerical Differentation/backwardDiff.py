83d3e544682f09f541c60acc4aa3ce992c4036d5
Syahril2401 <muhammadsyahril2406@gmail.com>
2025-05-26 22:05:35 +0700

    numDiff

---
+++ Numerical Differentation/backwardDiff.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+h = 0.1
+x = np.arange(0, 2*np.pi, h)
+y = np.cos(x)
+
+backward_diff = np.diff(y) / h
+x_diff = x[1:]
+exact_solution = -np.sin(x_diff)
+
+plt.figure(figsize=(12, 8))
+plt.plot(x_diff, backward_diff, '--', \
+         label='Finite difference approximation')
+plt.plot(x_diff, exact_solution, \
+         label='Exact solution')
+plt.legend()
+plt.show()
+
+max_error = max(abs(exact_solution - backward_diff))
+print(max_error)
